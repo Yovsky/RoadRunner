b3124d6e22bb6650af1bde0f3cff4ed673b95cbb
Yovsky <mouloudguenane651@gmail.com>
2026-06-28 17:14:09 +0100

    Add project files.

---
+++ RoadRunner/RoadRunner.cpp
@@ -0,0 +1,19 @@
+﻿#include "RoadRunner.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating a window", nullptr);
+		return 0;
+	}
+
+	SDL_Quit();
+	return 0;
+}
